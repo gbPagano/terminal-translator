189f6d1d4de9fdb81262c9a44f049613a0858069
gbPagano <guilhermebpagano@gmail.com>
2023-03-26 00:06:07 -0300

    cli tt command

--- src/main.py
+++ src/main.py
@@ -1,34 +1,46 @@
 from google.cloud import translate
-from dotenv import load_dotenv
-import os
+from typer import Typer, Argument, Option
+from .config import settings
+from typing import List
+import pyperclip
+from rich.console import Console
 
-load_dotenv()
+GOOGLE_APLICATION_CREDENTIALS = settings.google_aplication_credentials
+PROJECT_ID = settings.project_id
 
-GOOGLE_APPLICATION_CREDENTIALS = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
-PROJECT_ID = "metal-bus-381401"
+console = Console()
+app = Typer(add_completion=False)
 
-
-def translate_text(text, lang="en-US", source=None):
-
+@app.command()
+def translate_text(
+    text: List[str] = Argument(...,help="Text to be translated", show_default=False), 
+    target_lang: str = Option("en-US", "--target", "-t", help="Target language text to be translated"), 
+    source_lang: str = Option("", "--source", "-s", help="Source language of text to be translated"),
+    portuguese: bool = Option(False, "--pt", "-p", help="Text will be translated into portuguese"),
+    copy: bool = Option(False, "--copy", "-c", help="The output will be copied to clipboard"),
+):   
     client = translate.TranslationServiceClient()
     location = "global"
     parent = f"projects/{PROJECT_ID}/locations/{location}"
-
-    response = client.translate_text(
-        request={
-            "parent": parent,
-            "contents": [text],
-            "mime_type": "text/plain",
-            "source_language_code": source,
-            "target_language_code": lang,
-        }
-    )
+    if portuguese:
+        target_lang = "pt-BR"
+    
+    with console.status("Translating..."):
+        response = client.translate_text(
+            request={
+                "parent": parent,
+                "contents": [" ".join(text)],
+                "mime_type": "text/plain",
+                "source_language_code": source_lang,
+                "target_language_code": target_lang,
+            }
+        )
 
     for translation in response.translations:
-        print("Translated text: {}".format(translation.translated_text))
+        console.print(translation.translated_text)
+        if copy:
+            pyperclip.copy(translation.translated_text)
 
 
-translate_text("ola mundo")
-translate_text("always", lang="pt-BR")
-translate_text("papagaio")
-# translate_text_v2("pt-BR", "hello world!")
+if __name__ == "__main__":
+    app()
